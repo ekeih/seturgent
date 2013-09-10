d2fed91f68cb87b66838a6d51bebdbaa5759ce7d
Benjamin Oesterle <ben@freitagsrunde.org>
2013-09-10 19:51:54 +0200

    ignores special folders

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,12 +1,38 @@
 window.addEventListener("load", function(e) { 
     var notificationService = Components.classes["@mozilla.org/messenger/msgnotificationservice;1"].getService(Components.interfaces.nsIMsgFolderNotificationService);
-    notificationService.addListener(newMailListener, notificationService.msgAdded);
+    notificationService.addListener(msgListener, notificationService.msgAdded);
 }, false);
 
-var newMailListener = {
+var msgListener = {
     msgAdded: function(aMsgHdr) {
-        if( !aMsgHdr.isRead ) {
-            window.getAttention();
-        }
-    }
+    	processNewMail(aMsgHdr);
+	}
+}
+
+function processNewMail(aMsgHdr)
+{
+	if(!aMsgHdr.isRead) {
+		if(needsAttention(aMsgHdr.folder)) {
+			window.getAttention();
+		}
+	}
+}
+
+function needsAttention(folder)
+{
+	const flag = nsMsgFolderFlags;
+		
+	if(folder.isSpecialFolder(flag.Drafts,1)		||
+		folder.isSpecialFolder(flag.SentMail,1)		||
+		folder.isSpecialFolder(flag.Templates,1)	||
+		folder.isSpecialFolder(flag.Archive,1)		||
+		folder.isSpecialFolder(flag.Trash,1)		||
+		folder.isSpecialFolder(flag.Queue,1)		||
+		folder.isSpecialFolder(flag.Junk,1))		{
+		return false;
+	} 
+	else
+	{
+		return true;
+	}
 }
